4784e290fe51e133943ded3afc3e5fbaef255799
MatejMa2ur <matejmazur@outlook.com>
2025-04-02 00:10:04 +0200

    feat: add JSON ignore condition for null values in Validate
    - Applied `[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]` to `Validate` property of `StyleSetterOptions`.
    
    This ensures that null values for `Validate` are not serialized, improving payload efficiency. The change aligns with JSON serialization best practices and optimizes API communication.

--- Community.Blazor.MapLibre/Models/Sprite/StyleSetterOptions.cs
+++ Community.Blazor.MapLibre/Models/Sprite/StyleSetterOptions.cs
@@ -11,5 +11,6 @@ public class StyleSetterOptions
     /// Whether to check if the filter conforms to the MapLibre Style Specification. Disabling validation is a performance optimization that should only be used if you have previously validated the values you will be passing to this function.
     /// </summary>
     [JsonPropertyName("validate")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? Validate { get; set; }
 }
