ad6cf93f8afb1362cfe957ddeb9159e24eb0f7d8
MatejMa2ur <matejmazur@outlook.com>
2025-04-02 00:09:28 +0200

    feat: add support for padding and enhanced camera options
    - Introduced `PaddingOptions` model for map padding configurations.
    - Added `CameraForBoundsOptions` for bounds-based camera transitions.
    - Enhanced `CameraOptions` with properties like roll, elevation, and more.
    
    These changes extend functionality for managing map camera behavior and padding customization. All properties support JSON serialization, improving API integration and usability.

--- Community.Blazor.MapLibre/Models/Camera/CameraOptions.cs
+++ Community.Blazor.MapLibre/Models/Camera/CameraOptions.cs
@@ -1,11 +1,25 @@
+using System.Text.Json.Serialization;
+
 namespace Community.Blazor.MapLibre.Models.Camera;
 
 public class CameraOptions : ICameraOptions
 {
+    [JsonPropertyName("pitch")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? Pitch { get; set; }
+    [JsonPropertyName("roll")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? Roll { get; set; }
+    [JsonPropertyName("elevation")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? Elevation { get; set; }
+    [JsonPropertyName("center")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public LngLat? Center { get; set; }
+    [JsonPropertyName("zoom")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? Zoom { get; set; }
+    [JsonPropertyName("bearing")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public double? Bearing { get; set; }
 }
