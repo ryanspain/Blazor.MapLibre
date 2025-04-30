5998176cce357ea6fb45b608ec8f0ce558c00601
kjoergensen <kjoergensen@foreflight.com>
2025-04-30 11:26:04 +0200

    Fixes popup example

---
+++ Community.Blazor.MapLibre.Examples/Pages/Examples/CreatePopup.razor.cs
@@ -0,0 +1,41 @@
+﻿using Community.Blazor.MapLibre.Models;
+using Community.Blazor.MapLibre.Models.Event;
+
+namespace Community.Blazor.MapLibre.Examples.WebAssembly.Pages.Examples;
+
+public partial class CreatePopup
+{
+    private readonly MapOptions _mapOptions = new()
+    {
+        Style = "https://demotiles.maplibre.org/style.json"
+    };
+
+    private async Task OnMapLoad(EventArgs args)
+    {
+        if (_map is null)
+        {
+            return;
+        }
+
+        await _map.OnClick(null, e => AddPopup(e));
+
+        async void AddPopup(MapMouseEvent evnt)
+        {
+            await _map.CreatePopup(new Popup
+            {
+                Content = $"""
+                           <div>
+                               <h3>Map clicked</h3>
+                               <p>You clicked the map at <code>{evnt.LngLat.Latitude} {evnt.LngLat.Longitude}</code></p>
+                           </div>
+                           """,
+                Coordinates = evnt.LngLat
+            }, new PopupOptions
+            {
+                CloseOnClick = true,
+                CloseOnMove = true,
+                FocusAfterOpen = true,
+            });
+        }
+    }
+}
