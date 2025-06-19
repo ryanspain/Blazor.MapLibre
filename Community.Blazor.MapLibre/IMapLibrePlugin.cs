8e26508f4e5fab4d3ff76d354a3edfe6f94a1c86
Ryan Spain <8723433+ryanspain@users.noreply.github.com>
2025-06-19 23:25:56 +0100

    Add doc comments to IMapLibrePlugin

--- Community.Blazor.MapLibre/IMapLibrePlugin.cs
+++ Community.Blazor.MapLibre/IMapLibrePlugin.cs
@@ -4,5 +4,11 @@ namespace Community.Blazor.MapLibre;
 
 public interface IMapLibrePlugin : IAsyncDisposable
 {
+    /// <summary>
+    /// Initializes the plugin with the Map Libre map and JavaScript runtime.
+    /// Invoked by the <see cref="MapLibre"/> component when the map is ready.
+    /// </summary>
+    /// <param name="map">A reference to the Map Libre map object.</param>
+    /// <param name="runtime">A reference to the JavaScript runtime.</param>
     Task Initialize(IJSObjectReference map, IJSRuntime runtime);
 }
