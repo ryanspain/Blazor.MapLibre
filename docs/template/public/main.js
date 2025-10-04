6c3d3c07b0a399657176214e799f96216166c9d2
Ryan Spain <8723433+ryanspain@users.noreply.github.com>
2025-10-04 18:42:15 +0100

    Fix stylesheet and script paths in main.js

--- docs/template/public/main.js
+++ docs/template/public/main.js
@@ -10,12 +10,12 @@ export default {
         // Create a link element to import stylesheets
         let linkElement = document.createElement('link');
         linkElement.rel = 'stylesheet';
-        linkElement.href = '/_content/Community.Blazor.MapLibre/maplibre-5.3.0.min.css';
+        linkElement.href = '_content/Community.Blazor.MapLibre/maplibre-5.3.0.min.css';
         document.head.appendChild(linkElement);
         
         // Create a script element to load the Blazor WebAssembly runtime
         let blazorScriptElement = document.createElement('script');
-        blazorScriptElement.src = '/_framework/blazor.webassembly.js';
+        blazorScriptElement.src = '_framework/blazor.webassembly.js';
         blazorScriptElement.autostart = 'true';
         document.body.appendChild(blazorScriptElement);
     },
